4f8af6b0cab10f78c6c416a468f5301ddc5c514a
ssg8837 <ssg8837@gmail.com>
2021-06-24 02:08:02 +0900

    0624 sound, Tag, Collision

---
+++ Practice3D_03_Boost/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+  void OnCollisionEnter(Collision other) 
+  {
+      switch (other.gameObject.tag)
+      {
+          case "Friendly":
+            Debug.Log("Friendly");
+            break;
+          case "Fuel":
+            Debug.Log("Fuel");
+            break;
+          case "Finish":
+            Debug.Log("Finish");
+            break;
+          default:
+            Debug.Log("Damage!");
+            ReloadLevel();
+            break;
+      }      
+  }
+
+  void ReloadLevel()
+  {
+    int currentScene = SceneManager.GetActiveScene().buildIndex;
+    SceneManager.LoadScene(currentScene);
+  }
+}
--- Practice3D_03_Boost/Assets/Scripts/Movements.cs
+++ Practice3D_03_Boost/Assets/Scripts/Movements.cs
@@ -8,16 +8,20 @@ public class Movements : MonoBehaviour
     [SerializeField] float rotateThrust = 100f;
     private Rigidbody rb;
     private Transform tr;
+    private AudioSource aS;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         tr = GetComponent<Transform>();
+        aS = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        aS.mute =true;
         ProcessThrust();
         ProcessRotation();
     }
@@ -26,8 +30,14 @@ public class Movements : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            
+            aS.mute =false;
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            // if(!aS.isPlaying)
+            //     aS.Play();
         }
+        // else
+        //     aS.Stop();
     }
 
     private void ProcessRotation()
