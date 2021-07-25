b3f6811e93ea5857deb6e9da75b9068ab8339806
ssg8837 <ssg8837@gmail.com>
2021-07-26 08:21:55 +0900

    Restart LoadNextScene

--- Practice3D_03_Boost/Assets/Scripts/CollisionHandler.cs
+++ Practice3D_03_Boost/Assets/Scripts/CollisionHandler.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+  [SerializeField] float levelLoadDealy = 2f;
+
   void OnCollisionEnter(Collision other) 
   {
       switch (other.gameObject.tag)
@@ -10,22 +13,45 @@ public class CollisionHandler : MonoBehaviour
           case "Friendly":
             Debug.Log("Friendly");
             break;
-          case "Fuel":
-            Debug.Log("Fuel");
-            break;
           case "Finish":
             Debug.Log("Finish");
+            StartNextSequence();
             break;
           default:
             Debug.Log("Damage!");
-            ReloadLevel();
+            StartCrashSequence();//ReloadLevel();
             break;
       }      
   }
 
+  void StartCrashSequence()
+  {
+    //todo add SFX when Crash
+    //todo add Particle effect when Crash
+    GetComponent<Movements>().enabled = false;
+    Invoke("ReloadLevel", levelLoadDealy);
+  }
+
+  void StartNextSequence()
+  {
+    GetComponent<Movements>().enabled = false;
+    Invoke("LoadNextLevel", levelLoadDealy);
+  }
   void ReloadLevel()
   {
     int currentScene = SceneManager.GetActiveScene().buildIndex;
     SceneManager.LoadScene(currentScene);
+  }
+  void LoadNextLevel()
+  {
+    int currentScene = SceneManager.GetActiveScene().buildIndex;
+    if(SceneManager.sceneCount == currentScene+1)
+    {
+      SceneManager.LoadScene(currentScene+1);
+    }
+    else
+    {
+      SceneManager.LoadScene(0);
+    }
   }
 }
