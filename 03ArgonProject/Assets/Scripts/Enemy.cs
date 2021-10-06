de2c657dbe7177a1d758a1e18eb924210231ec64
ssg8837 <ssg8837@gmail.com>
2021-10-07 07:54:15 +0900

    Plus Ui

--- 03ArgonProject/Assets/Scripts/Enemy.cs
+++ 03ArgonProject/Assets/Scripts/Enemy.cs
@@ -6,9 +6,25 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVfx;
     [SerializeField] Transform parent;
-    private void OnParticleCollision(GameObject other) 
-    {   
-        GameObject vfx= Instantiate(deathVfx, transform.position, Quaternion.identity);
+
+    private void Start() 
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();    
+    }
+    ScoreBoard scoreBoard;
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit(100);
+        KillEnemy();
+    }
+
+    private void ProcessHit(int increaseAmount)
+    {
+        scoreBoard.IncreaseScore(increaseAmount);
+    }
+    private void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deathVfx, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         //gameObject(스크립트를 컴포넌트로 가지고 있는 게임 오브젝트)
         Destroy(gameObject);
