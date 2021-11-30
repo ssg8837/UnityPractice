18ec1170c13e21564a4ffa5614970f39e9108b6a
ssg8837 <ssg8837@gmail.com>
2021-12-01 01:31:11 +0900

    Enemy Smooth Move

--- RealmRush/Assets/Scripts/EnemyMoves.cs
+++ RealmRush/Assets/Scripts/EnemyMoves.cs
@@ -7,17 +7,33 @@ public class EnemyMoves : MonoBehaviour
 {
     [SerializeField] List<WayPoint> path = new List<WayPoint>();
     // Start is called before the first frame update
+    [SerializeField] [Range(0f, 5f)] float speed = 1f; 
     void Start()
     {
-        StartCoroutine(PrintWayPointName());
+        StartCoroutine(FollowPath());
     }
 
-   IEnumerator PrintWayPointName()
+   IEnumerator FollowPath()
     {
         foreach(WayPoint wayPoint in path)
         {
-          transform.position = wayPoint.transform.position;
-          yield return new WaitForSeconds(1f);
+            //시작점
+            Vector3 startPosition = transform.position;
+            //도착점
+            Vector3 endPosition = wayPoint.transform.position;
+            //보간시 사용하는 퍼센트
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            //100퍼까지
+            while(travelPercent < 1f) 
+            {
+                travelPercent += Time.deltaTime * speed;
+                //선형보간
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
     // // Update is called once per frame
