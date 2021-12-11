6c4a443e1ec0958383164b5217db720d474fb56c
ssg8837 <ssg8837@gmail.com>
2021-12-12 02:21:02 +0900

    Pure C# Code Serializable

---
+++ RealmRush/Assets/PathFinding/Node.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// MonoBehaviour 를 사용하지 않음으로써 드래그해서 붙여넣는건 불가능해짐
+[System.Serializable]
+public class Node //: MonoBehaviour
+{
+    public Vector2Int coordinates;
+    public bool isWalkable;
+    public bool isExplored;
+    public bool isPath;
+    public Node connectionTo;
+
+    public Node(Vector2Int coordinates, bool isWalkabe)
+    {
+        this.coordinates = coordinates;
+        this.isWalkable = isWalkabe;
+
+    }
+
+}
