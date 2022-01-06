c6e83b3168b25ff0d678c874c9af4ac1db04d8a0
ssg8837 <ssg8837@gmail.com>
2022-01-06 22:28:59 +0900

    Zombie Runner Start

---
+++ ZombieRunner/Assets/Script/EnemyAi.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
