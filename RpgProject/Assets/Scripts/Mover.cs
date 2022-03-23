d2a1019a90d936cf3fdf126e9b9ddb0f02b6be3f
ssg8837 <ssg8837@gmail.com>
2022-03-24 01:52:02 +0900

    NAV메쉬 + 마우스 클릭 이동

--- RpgProject/Assets/Scripts/Mover.cs
+++ RpgProject/Assets/Scripts/Mover.cs
@@ -11,6 +11,7 @@ public class Mover : MonoBehaviour
     // 길을 찾아서 이동할 에이전트
     private NavMeshAgent navAgent;
 
+
     private void Awake() 
     {
         // 게임이 시작되면 게임 오브젝트에 부착된 NavMeshAgent 컴포넌트를 가져와서 저장
@@ -20,7 +21,27 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        // 에이전트에게 목적지를 알려주는 함수        
-        navAgent.SetDestination(target.position);
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+
+    }
+
+    private void MoveToCursor()
+    {
+        //마우스 포지션으로 레이캐스트
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //레이캐스트에 맞은 정보
+        RaycastHit hit;
+        // 맞았는가 판정하면서 hit에 정보 저장.
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        //레이캐스트에 맞은 정보가 있을 경우
+        if(hasHit)
+        {
+            // 에이전트에게 목적지를 알려주는 함수        
+            navAgent.SetDestination(hit.point);
+        }
     }
 }
